6a58a90e060b4a3c0d1a4072b88abda6b90048c9
Aditya <adityafav@gmail.com>
2022-11-29 17:24:32 +0530

    feat : adds dotenv

--- app.js
+++ app.js
@@ -1,80 +1,73 @@
-var createError   = require('http-errors');
-var express       = require('express');
-var path          = require('path');
-var cookieParser  = require('cookie-parser');
-var logger        = require('morgan');
-var flash         = require("connect-flash");
-var mongoose      = require('mongoose');
-var passport      = require("passport");
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var flash = require("connect-flash");
+var mongoose = require("mongoose");
+var passport = require("passport");
 var LocalStrategy = require("passport-local");
-var User          = require("./models/user");
+var User = require("./models/user");
+require("dotenv").config();
 
-
-var indexRouter = require('./routes/index');
-
+var indexRouter = require("./routes/index");
 
 var app = express();
-mongoose.connect("mongodb+srv://aditya:aditya@cluster0.3xhxi.mongodb.net/hospitalapp?retryWrites=true&w=majority",{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
-
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(flash());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // passport config
-app.use(require("express-session")({
-  secret:"SECRET",
-  resave: false,
-  saveUninitialized: false
-}))
+app.use(
+  require("express-session")({
+    secret: "SECRET",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate())); //passport authenticate middleware
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   res.locals.error = req.flash("error");
   res.locals.success = req.flash("success");
   next();
-})
-
+});
 
 // routes config
-app.use('/', indexRouter);
-
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- middleware/index.js
+++ middleware/index.js
@@ -1,26 +1,23 @@
-var multer      = require('multer');
+var multer = require("multer");
 
 // multer config
-var storage = multer.diskStorage({ 
-  destination: (req, file, cb) => { 
-    cb(null, './public/uploads/'); 
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/uploads/");
   },
-  filename: (req, file, cb) => { 
-      cb(null, new Date().toISOString() + file.originalname); 
-    } 
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + file.originalname);
+  },
 });
 
-
 module.exports = {
-  
-  isLoggedIn : function(req, res, next) {
-    if(req.isAuthenticated()){
-        return next();
+  isLoggedIn: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
     }
     req.flash("error", "You need to be logged in to do that.");
     res.redirect("/login");
   },
 
-  upload : multer({ storage: storage })
-
+  upload: multer({ storage: storage }),
 };
